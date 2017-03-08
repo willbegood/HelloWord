e71c6102397e578c1b1ab2732b4a3baa32830073
willbegood <andreyblb97@mail.ru>
2017-03-08 12:39:17 +0300

    Initial commit

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by Андрей on 02.03.2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
